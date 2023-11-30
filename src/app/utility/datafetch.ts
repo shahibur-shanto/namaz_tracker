8c4963ed94700c0a686c1dab0e3be4186fcab0f2
shahibur-shanto <shahibur407@gmail.com>
2023-11-30 14:59:48 +0600

    data update done

--- src/app/utility/datafetch.ts
+++ src/app/utility/datafetch.ts
@@ -1,26 +1,26 @@
-export const fetchPrayerData = async (currentYear, currentMonth, city, country, method) => {
-    try {
-      const prayerTime = await fetch(
-        `https://api.aladhan.com/v1/calendarByCity/${currentYear}/${currentMonth}?city=${city}&country=${country}&method=${method}`
-      );
-      const prayer = await prayerTime.json();
+// export const fetchPrayerData = async (currentYear, currentMonth, city, country, method) => {
+//     try {
+//       const prayerTime = await fetch(
+//         `https://api.aladhan.com/v1/calendarByCity/${currentYear}/${currentMonth}?city=${city}&country=${country}&method=${method}`
+//       );
+//       const prayer = await prayerTime.json();
   
-      prayer.data.forEach((item) => {
-        const newTimings = {};
-        Object.entries(item.timings).forEach(([timingKey, timingValue]) => {
-          newTimings[timingKey] = {
-            time: `${timingValue}`,
-            isComplete: false,
-            isLateComplete: false,
-          };
-        });
-        item.timings = newTimings;
-      });
+//       prayer.data.forEach((item) => {
+//         const newTimings = {};
+//         Object.entries(item.timings).forEach(([timingKey, timingValue]) => {
+//           newTimings[timingKey] = {
+//             time: `${timingValue}`,
+//             isComplete: false,
+//             isLateComplete: false,
+//           };
+//         });
+//         item.timings = newTimings;
+//       });
   
-      localStorage.setItem("prayerTime", JSON.stringify(prayer.data));
-      return prayer.data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      throw error;
-    }
-  };
+//       localStorage.setItem("prayerTime", JSON.stringify(prayer.data));
+//       return prayer.data;
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//       throw error;
+//     }
+//   };
